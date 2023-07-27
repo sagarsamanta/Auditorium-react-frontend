e9a3cd9a7027de32cd976af1e60abaa7cd7bd438
Sagar Samanta <sagar.samanta@dignizant.com>
2023-07-27 15:30:01 +0530

    fix table width

---
+++ src/components/DataTableUsersBookings.jsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+import DataTable from "react-data-table-component";
+import { displayDate } from "../lib/utils";
+
+const DataTableUsersBookings = ({ data, className }) => {    
+    const columns = [
+        {
+            name: 'Date',
+            cell: row => <>{displayDate(row?.showtime?.movie?.releaseDate)}</>,
+        },
+        {
+            name: 'Movie',
+            // minWidth:'300px',
+            cell: row => (
+              <span className="">
+                {row?.movie?.title}
+              </span>
+            ),
+          },
+        {
+            name: 'Start Time',
+            cell: row => <>{row?.showtime?.showStartTime}</>,
+        },
+        {
+            name: 'End Time',
+            cell: row => <>{row?.showtime?.showEndTime}</>,
+        },
+        {
+            name: 'Seats',
+            cell: row => <>{row?.showtime?.bookedSeats}</>,
+        },
+        {
+            name: 'Booked seats',
+            // minWidth:'300px',
+            cell: row => {
+                const allSeats=row?.seats?.map((a)=>a.seatNo).join(", ")
+                return allSeats
+            },
+        },
+        {
+            name: 'Total Amount',
+            cell: row => <>{row?.totalPrice}</>,
+            minWidth:'300px'
+        },
+        
+        // {
+        //     name: 'Action',
+        //     selector: row => (
+        //         < className="space-x-4">
+        //             <Link to={`/admin/users/bookings/movie/${row._id}`} className="text-xs inline-block py-2 px-4 rounded-lg transition duration-200 border border-blue-600 w-24 text-center text-blue-600 font-serif hover:bg-blue-600 hover:text-white">View</Link>
+
+
+        //         </>
+        //     ),
+        // },
+    ];
+    return (
+        <>
+            <DataTable
+                columns={columns}
+                data={data}
+                className={className}
+                pagination
+            />
+        </>
+    )
+}
+
+export default DataTableUsersBookings;
