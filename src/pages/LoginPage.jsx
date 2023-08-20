4e0dcb4d88faada3c369b2e7536b8d5ac94931d4
Tanuj Patra <tanujpatra228@gmail.com>
2023-08-20 14:10:29 +0530

    Fix login page style issues

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,9 +6,9 @@ const LoginPage = () => {
     return (
         <>
             <section className="flex items-center min-h-screen mx-auto lg:justify-center px-4 sm:px-6 lg:px-8">
-                <div className="flex flex-col overflow-hidden rounded-md shadow-lg max md:flex-row md:flex-1 lg:max-w-screen-md">
+                <div className="flex flex-col overflow-hidden rounded-md shadow-lg w-full md:flex-row md:flex-1 lg:max-w-screen-md">
                     <div className="p-4 py-6 text-skin-muted bg-gray-800 md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly">
-                        <div className="my-3 text-4xl font-bold tracking-wider text-center text-skin-inverted">
+                        <div className="my-3 text-4xl font-bold tracking-wider text-center text-skin-inverted flex justify-center items-center">
                             <Logo />
                         </div>
                         <p className="mt-6 font-normal text-center md:mt-0">
