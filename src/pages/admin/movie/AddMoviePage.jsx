f46c5fae7b03406fc86f7eff39bfbbef9b9e8d9d
Tanuj G. Patra <tanujpatra228@gmail.com>
2023-08-25 22:43:53 +0530

    Added Back buttons

--- src/pages/admin/movie/AddMoviePage.jsx
+++ src/pages/admin/movie/AddMoviePage.jsx
@@ -1,15 +1,30 @@
+import { useNavigate } from 'react-router-dom';
 import { Link, useLocation } from 'react-router-dom';
 import AddMovieForm from '../../../components/admin/AddMovieForm';
+import { RiArrowGoBackLine } from 'react-icons/ri';
 
 const AddMoviePage = () => {
     const { state } = useLocation();
-    console.log('state', state);
+    const navigate = useNavigate();
     return (
         <>
             <main className='movies-page'>
                 <div className="flex justify-between items-center">
                     <h1 className="text-xl md:text-2xl lg:text:3xl">{state?.movie ? 'Repeat Movie' : 'Add New Movie'}</h1>
-                    <Link to="/admin/movies" className="text-sm inline-block px-2 py-2 md:px-4 md:py-3 rounded-lg bg-skin-base text-skin-inverted">See All Movie</Link>
+                    <div className='flex items-center gap-2'>
+                        <button
+                            className="text-sm px-2 py-2 md:px-4 md:py-3 rounded-lg border border-skin-base bg-skin-inverted text-skin-base flex items-center gap-2"
+                            onClick={() => navigate(-1)}
+                        >
+                            <RiArrowGoBackLine /> Back
+                        </button>
+                        <button
+                            className="text-sm inline-block px-2 py-2 md:px-4 md:py-3 rounded-lg bg-skin-base text-skin-inverted"
+                            onClick={() => navigate(-1)}
+                        >
+                            See All Movie
+                        </button>
+                    </div>
                 </div>
                 <div className="movies-table-wrapper">
                     <AddMovieForm addMovie={state?.movie} />
--- src/pages/admin/movie/MoviesPage.jsx
+++ src/pages/admin/movie/MoviesPage.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import DataTableMovie from "../../../components/DataTableMovie";
-import { getAllMovies } from "../../../lib/utils";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Loader from "../../../components/UI/Loader";
 import { useAuth } from "../../../lib/hooks/useAuth";
 import Axios from "../../../lib/axiosInstance";
+import { RiArrowGoBackLine } from "react-icons/ri";
 
 const MoviesPage = () => {
     const queryParameters = new URLSearchParams(window.location.search);
@@ -16,7 +16,6 @@ const MoviesPage = () => {
 
     useEffect(() => {
         const response = async () => {
-            // const data = await getAllMovies(token);
             Axios("GET", `/movie/all-movie-by-title/${movieTitle}`, null, { authRequest: true, token: token })
                 .then(res => {
                     console.log('res', res);
@@ -37,15 +36,20 @@ const MoviesPage = () => {
             <main className="movies-page  ">
                 <div className="flex justify-between items-center p-4 border border-slate-100 rounded-md shadow-md">
                     <h1 className="text-xl md:text-2xl lg:text:3xl">Movies</h1>
-                    <button
-                        to="/admin/movies/add"
-                        className="text-sm inline-block px-2 py-2 md:px-4 md:py-3 rounded-lg bg-skin-base text-skin-inverted"
-                        onClick={() => navigate("/admin/movies/add", { state: { movie: movies[0] } })}
-                    >
-                        <div className="flex gap-2 items-center">
-                            <div>Repeat Movie</div>
-                        </div>
-                    </button>
+                    <div className="flex items-center gap-2">
+                        <button
+                            className="text-sm px-2 py-2 md:px-4 md:py-3 rounded-lg border border-skin-base bg-skin-inverted text-skin-base flex items-center gap-2"
+                            onClick={() => navigate(-1)}
+                        >
+                            <RiArrowGoBackLine /> Back
+                        </button>
+                        <button
+                            className="text-sm inline-block px-2 py-2 md:px-4 md:py-3 rounded-lg bg-skin-base text-skin-inverted"
+                            onClick={() => navigate("/admin/movies/add", { state: { movie: movies[0] } })}
+                        >
+                            Repeat Movie
+                        </button>
+                    </div>
                 </div>
 
                 <div className="movies-table-wrapper p-4 shadow-md mt-5">
