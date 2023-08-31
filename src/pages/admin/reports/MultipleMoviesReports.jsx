9e4dc475b3d6fd6e721a1a9c543b600c1d8aa73a
Sagar Samanta <sagar.samanta@dignizant.com>
2023-08-31 08:56:36 +0530

    positioned total amount

--- src/pages/admin/reports/MultipleMoviesReports.jsx
+++ src/pages/admin/reports/MultipleMoviesReports.jsx
@@ -12,9 +12,9 @@ const MultipleMoviesReports = ({ data }) => {
             {/* <th className="border border-gray-300 px-4 py-2">Movie Title</th> */}
             <th className="border border-gray-300 px-4 py-2">Total Booking</th>
             <th className="border border-gray-300 px-4 py-2">Total Reserved Seats</th>
-            <th className="border border-gray-300 px-4 py-2">Total Amount Collected</th>
             <th className="border border-gray-300 px-4 py-2">Total Amount Online</th>
             <th className="border border-gray-300 px-4 py-2">Total Amount Cash</th>
+            <th className="border border-gray-300 px-4 py-2">Total Amount Collected</th>
           </tr>
         </thead>
         <tbody>
@@ -22,9 +22,9 @@ const MultipleMoviesReports = ({ data }) => {
             {/* <td className="border border-gray-300 px-4 py-2">{movieTitle}</td> */}
             <td className="border border-gray-300 px-4 py-2  text-center">{data?.bookedSeats}</td>
             <td className="border border-gray-300 px-4 py-2 text-center ">{data?.reservedSeats}</td>
-            <td className="border border-gray-300 px-4 py-2 text-center">{data?.totalAmountCollected?.total && getCurrencyFormat(data?.totalAmountCollected?.total)} </td>
             <td className="border border-gray-300 px-4 py-2 text-center">{data?.totalAmountCollected?.online && getCurrencyFormat(data?.totalAmountCollected?.online)}</td>
             <td className="border border-gray-300 px-4 py-2 text-center">{data?.totalAmountCollected?.cash && getCurrencyFormat(data?.totalAmountCollected?.cash)}</td>
+            <td className="border border-gray-300 px-4 py-2 text-center">{data?.totalAmountCollected?.total && getCurrencyFormat(data?.totalAmountCollected?.total)} </td>
           </tr>
         </tbody>
       </table>
